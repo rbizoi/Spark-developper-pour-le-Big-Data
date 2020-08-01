f3e01bade33d2d51f30e933bc9a17b047cea4a70
rbizoi <razvan.bizoi@gmail.com>
2020-08-01 19:08:27 +0200

    Update 03-core_site_xml.sh

--- Chapitre-02/Scripts-Installation-Un-Serveur/01-Hadoop/03-core_site_xml.sh
+++ Chapitre-02/Scripts-Installation-Un-Serveur/01-Hadoop/03-core_site_xml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $USER != "hdfs" ]; then
-        echo "Le script doit être exécuté en tant qu'utilisateur: hdfs"
+if [ $USER != "hadoop" ]; then
+        echo "Le script doit être exécuté en tant qu'utilisateur: hadoop"
         exit -1
 fi
 
@@ -52,7 +52,7 @@ cat <<FIN_FICHIER > $HADOOP_HOME/etc/hadoop/core-site.xml
 
     <property>
       <name>hadoop.http.staticuser.user</name>
-      <value>hdfs</value>
+      <value>hadoop</value>
     </property>
 
     <property>
