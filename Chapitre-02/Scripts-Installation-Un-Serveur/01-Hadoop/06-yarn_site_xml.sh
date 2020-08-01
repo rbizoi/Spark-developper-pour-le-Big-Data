1ff0e9784b56bcac4164e9c68702e7335d317b54
rbizoi <razvan.bizoi@gmail.com>
2020-08-01 19:08:38 +0200

    Update 06-yarn_site_xml.sh

--- Chapitre-02/Scripts-Installation-Un-Serveur/01-Hadoop/06-yarn_site_xml.sh
+++ Chapitre-02/Scripts-Installation-Un-Serveur/01-Hadoop/06-yarn_site_xml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $USER != "hdfs" ]; then
-        echo "Le script doit être exécuté en tant qu'utilisateur: hdfs"
+if [ $USER != "hadoop" ]; then
+        echo "Le script doit être exécuté en tant qu'utilisateur: hadoop"
         exit -1
 fi
 
