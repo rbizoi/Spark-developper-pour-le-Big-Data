ee36d14715554c7eca134727159698f91af65f80
rbizoi <razvan.bizoi@gmail.com>
2020-08-02 22:37:46 +0200

    Update 01-installation-zeppelin.sh

--- Chapitre-02/Scripts-Installation-Un-Serveur/06-Zeppelin/01-installation-zeppelin.sh
+++ Chapitre-02/Scripts-Installation-Un-Serveur/06-Zeppelin/01-installation-zeppelin.sh
@@ -9,13 +9,13 @@ cd ~
 
 
 wget https://edef4.pcloud.com/cfZbVwv0ZIqIWJZvQYZZ76Qm37Z2ZZRR5ZZydJMU7Zp7ZJ7Z2ZuZqZ47ZTZtZaZQ7ZrZhZDZNZuFKRQgDk125Ch3HX85t2opvYmm5k/zeppelin-0.9.0-SNAPSHOT.tar.gz
-
 tar xzvf zeppelin-0.9.0-SNAPSHOT.tar.gz
 mv zeppelin-0.9.0-SNAPSHOT zeppelin
 rm zeppelin-0.9.0-SNAPSHOT.tar.gz
 sudo mv zeppelin /usr/share
 
 bash <(curl -s https://raw.githubusercontent.com/rbizoi/Spark-developper-pour-le-Big-Data/master/Chapitre-02/Scripts-Installation-Un-Serveur/04-Zookeeper/02-zeppelin_site_xml.sh)
+bash <(curl -s https://raw.githubusercontent.com/rbizoi/Spark-developper-pour-le-Big-Data/master/Chapitre-02/Scripts-Installation-Un-Serveur/04-Zookeeper/03-zeppelin_env_sh.sh)
 
 sudo chown -R zeppelin:hadoop /usr/share/zeppelin
 ll /usr/share/zeppelin
@@ -42,4 +42,4 @@ zeppelin-daemon.sh status
 zeppelin-daemon.sh restart
 zeppelin-daemon.sh stop
 
-sudo netstat -lnp | grep 9995
+sudo netstat -lnp | grep 9999
