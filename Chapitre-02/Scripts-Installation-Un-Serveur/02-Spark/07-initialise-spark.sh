43468084d98cf2fc0fe7ecf317a3199d928b22a6
rbizoi <razvan.bizoi@gmail.com>
2020-08-02 16:59:54 +0200

    Update 07-initialise-spark.sh

--- Chapitre-02/Scripts-Installation-Un-Serveur/02-Spark/07-initialise-spark.sh
+++ Chapitre-02/Scripts-Installation-Un-Serveur/02-Spark/07-initialise-spark.sh
@@ -9,6 +9,7 @@ hdfs dfs -mkdir /spark-jars
 hdfs dfs -mkdir /spark-history
 hdfs dfs -put $SPARK_HOME/jars/* /spark-jars
 hdfs dfs -ls /spark-jars
+hdfs dfs -mkdir /user/spark/entrepot-de-donnees
 
 $SPARK_HOME/sbin/start-all.sh
 jps
