fe8dc64249348aadaa3f75305acfe612d65dbb24
rbizoi <razvan.bizoi@gmail.com>
2020-08-02 21:50:44 +0200

    Update 01-installation-zeppelin.sh

--- Chapitre-02/Scripts-Installation-Un-Serveur/06-Zeppelin/01-installation-zeppelin.sh
+++ Chapitre-02/Scripts-Installation-Un-Serveur/06-Zeppelin/01-installation-zeppelin.sh
@@ -7,8 +7,41 @@ fi
 
 cd ~
 
-wget wget https://mega.nz/file/OOhBjZBJ#8Ocuwx7u67I_ci74pnKzaHwaBUtUeispYxT4jzpDGec
-tar xzvf kafka_2.12-2.5.0.tgz
-rm -f kafka_2.12-2.5.0.tgz
-mv kafka_2.12-2.5.0 kafka
-mv kafka /usr/share
+
+#wget ...
+tar xzvf zeppelin-0.9.0-SNAPSHOT.tar.gz
+mv zeppelin-0.9.0-SNAPSHOT zeppelin
+rm zeppelin-0.9.0-SNAPSHOT.tar.gz
+sudo mv zeppelin /usr/share
+
+cp $ZEPPELIN_HOME/conf/zeppelin-site.xml.template $ZEPPELIN_HOME/conf/zeppelin-site.xml
+
+
+
+
+sudo chown -R zeppelin:hadoop /usr/share/zeppelin
+ll /usr/share/zeppelin
+
+sudo rm -Rf /var/log/zeppelin
+sudo mkdir /var/log/zeppelin
+sudo mkdir -p /var/run/zeppelin/zeppelin
+sudo chown -R zeppelin:hadoop /var/*/zeppelin
+
+ll /var/*/zeppelin
+
+
+cat << FIN_FICHIER > /etc/profile.d/zeppelin.sh
+#!/bin/bash
+# Configuration Zeppelin
+export ZEPPELIN_HOME=/usr/share/zeppelin
+export PATH=\$ZEPPELIN_HOME/bin:\$PATH
+FIN_FICHIER
+
+
+zeppelin-daemon.sh start
+zeppelin-daemon.sh start
+zeppelin-daemon.sh status
+zeppelin-daemon.sh restart
+zeppelin-daemon.sh stop
+
+sudo netstat -lnp | grep 9995
