cd83f3d69b1c2200db3a4d20d89ec8007559e971
rbizoi <razvan.bizoi@gmail.com>
2020-08-02 02:21:00 +0200

    Update 01-installation-mysql.sh

--- Chapitre-02/Scripts-Installation-Un-Serveur/02-Hive/01-installation-mysql.sh
+++ Chapitre-02/Scripts-Installation-Un-Serveur/02-Hive/01-installation-mysql.sh
@@ -4,3 +4,19 @@ if [ $USER != "hdfs" ]; then
         echo "Le script doit être exécuté en tant qu'utilisateur: hdfs"
         exit -1
 fi
+
+mysql_secure_installation <<FIN_FICHIER
+
+y
+CoursSPARK#
+CoursSPARK#
+y
+y
+y
+y
+FIN_FICHIER
+
+sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+cat /etc/mysql/mysql.conf.d/mysqld.cnf  |grep bind-address
+
+systemctl restart mysql
