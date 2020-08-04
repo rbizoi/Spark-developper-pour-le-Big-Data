4bd9a4841527dbb4f1553f8659caf82603c261ee
rbizoi <razvan.bizoi@gmail.com>
2020-08-04 14:47:10 +0200

    Update 02-stop-cluster.sh

--- Chapitre-02/Scripts-Installation-Un-Serveur/08-Demarrage-Serveur/02-stop-cluster.sh
+++ Chapitre-02/Scripts-Installation-Un-Serveur/08-Demarrage-Serveur/02-stop-cluster.sh
@@ -7,8 +7,14 @@ fi
 
 cd ~
 
+systemctl stop kafka
+sleep 5
+systemctl stop zookeeper
+sleep 5
+systemctl stop zeppelin
+sleep 5
+su -c /usr/share/spark/sbin/stop-all.sh - spark
+sleep 5
 su -c /usr/share/hadoop/sbin/stop-yarn.sh - hdfs
 sleep 5
 su -c /usr/share/hadoop/sbin/stop-dfs.sh - hdfs
-sleep 5
-su -c /usr/share/spark/sbin/stop-all.sh - spark
