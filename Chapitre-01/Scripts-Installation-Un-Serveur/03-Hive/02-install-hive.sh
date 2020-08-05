95507b362c10774f3b50570ec9bad4242bbf66d3
rbizoi <razvan.bizoi@gmail.com>
2020-08-05 17:53:19 +0200

    Update 02-install-hive.sh

--- Chapitre-01/Scripts-Installation-Un-Serveur/03-Hive/02-install-hive.sh
+++ Chapitre-01/Scripts-Installation-Un-Serveur/03-Hive/02-install-hive.sh
@@ -14,7 +14,7 @@ export HADOOP_HOME=/usr/share/hadoop
 export HIVE_CONF_DIR=/usr/share/spark/conf
 FIN_FICHIER
 
-bash <(curl -s https://raw.githubusercontent.com/rbizoi/Spark-developper-pour-le-Big-Data/master/Chapitre-02/Scripts-Installation-Un-Serveur/03-Hive/03-hive_site_xml.sh)
+bash <(curl -s https://raw.githubusercontent.com/rbizoi/Spark-developper-pour-le-Big-Data/master/Chapitre-01/Scripts-Installation-Un-Serveur/03-Hive/03-hive_site_xml.sh)
 
 wget https://cdn.mysql.com//Downloads/Connector-J/mysql-connector-java-8.0.20.tar.gz
 tar -xvzf mysql-connector-java-8.0.20.tar.gz
