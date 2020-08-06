fd379533fa8edb08b44807b1084644bb4fd041ef
rbizoi <razvan.bizoi@gmail.com>
2020-08-06 18:50:26 +0200

    Update controle-spark-scala.scala

--- Chapitre-01/Scripts-Installation-Un-Serveur/02-Spark/controle-spark-scala.scala
+++ Chapitre-01/Scripts-Installation-Un-Serveur/02-Spark/controle-spark-scala.scala
@@ -47,16 +47,16 @@ val sqlfV = udf(parseVille)
 
 val donneesStations = spark.read.json("donnees/json/postesSynop.json")
 val postesMeteo     = donneesStations.
-                       withColumn("Nom", sqlfV(donneesStations("Nom")))                       
-postesMeteo .createOrReplaceTempView("PostesMeteo")
-spark.sql("select * from PostesMeteo").show(5)
+                       withColumn("Nom", sqlfV(donneesStations("Nom")))
+postesMeteo.createOrReplaceTempView("PostesMeteo")
+spark.sql("select * from PostesMeteo limit 3").show()
 spark.sql("show databases").show()
 spark.sql("use cours_spark")
-spark.sql("show tabless").show()
+spark.sql("show tables").show()
 spark.sql("DROP TABLE IF EXISTS cours_spark.postesmeteo")
-postesMeteo.saveAsTable("postesmeteo")
-spark.sql("show tabless").show()
-spark.sql("select * from cours_spark.postesmeteo where ID < 8000 limit 5").show()
+postesMeteo.write.saveAsTable("postesmeteo")
+spark.sql("show tables").show()
+spark.sql("select * from cours_spark.postesmeteo where ID < 8000 limit 3").show()
 
 
 
