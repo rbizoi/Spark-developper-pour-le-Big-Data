238f04a0c18f054fbec7c3bb17f3cf610ca683d5
rbizoi <razvan.bizoi@gmail.com>
2020-08-07 19:51:21 +0200

    Create 01-machine-learning.py

---
+++ Chapitre-02/1.3-machine-learning/01-machine-learning.py
@@ -0,0 +1,38 @@
+from pyspark.ml import Pipeline
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml.feature    import PCA, StandardScaler, VectorAssembler
+from pyspark.ml.linalg     import array
+
+
+donnees = spark.sql("select * from cours_spark.meteoMensuelle order by 1").cache()
+
+modelA = VectorAssembler().\
+                setInputCols(
+                       ['Janvier', 'Fevrier', 'Mars',
+                        'Avril', 'Mai', 'Juin', 'Juillet',
+                        'Aout', 'Septembre', 'Octobre',
+                        'Novembre', 'Decembre']).\
+                 setOutputCol('variables')
+
+modelN = StandardScaler().\
+                setInputCol("variables").\
+                setOutputCol("vNormalisees").\
+                setWithStd(True).\
+                setWithMean(False)
+
+modelACP = PCA().\
+            setInputCol("vNormalisees").\
+            setOutputCol("vACP").\
+            setK(2)
+
+modelKM = KMeans().setK(7).\
+                   setFeaturesCol("vACP").\
+                   setPredictionCol("vKM")
+
+modelPipe = Pipeline(stages=[modelA, modelN,
+              modelACP, modelKM]).fit(donnees)
+
+donneesKM = modelPipe.transform(donnees)
+
+donneesKM.select("Ville","vKM").show(5)
