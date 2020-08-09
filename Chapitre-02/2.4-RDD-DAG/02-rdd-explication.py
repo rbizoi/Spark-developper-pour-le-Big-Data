4ea82213075280ddeac35f2f9916c4cb190a2f3c
rbizoi <razvan.bizoi@gmail.com>
2020-08-09 14:29:00 +0200

    Update 02-rdd-explication.py

--- Chapitre-02/2.4-RDD-DAG/02-rdd-explication.py
+++ Chapitre-02/2.4-RDD-DAG/02-rdd-explication.py
@@ -1,3 +1,65 @@
+clear
+export PYSPARK_DRIVER_PYTHON=python3
+export PYSPARK_DRIVER_PYTHON_OPTS=''
+pyspark --master spark://jupiter.olimp.fr:7077
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode,split,length,lower,regexp_replace
+
+spark = SparkSession.builder.\
+        appName("Lecture  du flux des tweets").getOrCreate()
+
+spark.conf.set('spark.sql.shuffle.partitions', 8)
+spark.conf.set('spark.default.parallelism', 8)
+
+
+from pyspark.sql.types import StructType, \
+     StructField, FloatType, \
+     IntegerType, StringType
+
+def transformLigne(ligne):
+   champs = ligne.split(";")
+   return  (    str(champs[0]),
+                int(str(champs[1])[0:4]),
+                int(str(champs[1])[4:6]),
+                int(str(champs[1])[6:8]),
+                float(str(champs[7])) - 273.15,
+                float(int(str(champs[9])) / 100 ),
+                int(str(champs[10])),
+                float(int(str(champs[20])) / 1000 )   )
+
+donnees = spark.sparkContext. \
+       textFile('/user/spark/donnees/meteo'). \
+       filter( lambda ligne : str(ligne)[0:5].isdigit()). \
+       map(lambda ligne: str(ligne).replace('mq','0')). \
+       map(lambda ligne: transformLigne(ligne)). \
+       persist()
+
+donnees.count()
+donnees.take(2)
+
+schema = StructType([
+            StructField('station'     , StringType() , True),
+            StructField('annee'       , IntegerType(), True),
+            StructField('mois'        , IntegerType(), True),
+            StructField('jour'        , IntegerType(), True),
+            StructField('temperature' , FloatType()  , True),
+            StructField('humidite'    , FloatType(), True),
+            StructField('visibilite'  , IntegerType(), True),
+            StructField('pression'    , FloatType()  , True)])
+
+donneesStations = spark.createDataFrame(donnees, schema)
+donneesStations.show(3)
+exit()
+
+
+
+
+
+
+
+
 import org.apache.spark.sql.SparkSession
 
 object donneesMeteo {
