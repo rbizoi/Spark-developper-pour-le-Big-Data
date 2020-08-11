f97b9beb9b42dd43409aa480f488917f4f726078
rbizoi <razvan.bizoi@gmail.com>
2020-08-11 19:06:54 +0200

    Update 01-creation-rdd.scala

--- Chapitre-03/01-Les-API-de-bas-niveau/01-creation-rdd.scala
+++ Chapitre-03/01-Les-API-de-bas-niveau/01-creation-rdd.scala
@@ -12,8 +12,6 @@ def printConfigs(session: SparkSession) = {
 spark.conf.set("spark.default.parallelism",8)
 printConfigs(spark)
 
-
-
 val data = Array(("Ajaccio",653    ),
                  ("Angers",690     ),
                  ("Angoulème",826  ),
@@ -48,15 +46,6 @@ val data = Array(("Ajaccio",653    ),
                  ("Toulouse",656   ),
                  ("Tours",687      ),
                  ("Vichy",761      ))
-
-val data = Array(("Ajaccio",653    ),
-                ("Angoulème",826  ),
-                ("Besançon",1088  ),
-                ("Biarritz",1474  ),
-                ("Bordeaux",947   ),
-                ("Brest",1157     ),
-                ("Caen",713       ),
-                ("Strasbourg",719 ))
 
 val rdd01 = spark.sparkContext.parallelize(data)
 val rdd02 = spark.sparkContext.parallelize(data,1)
