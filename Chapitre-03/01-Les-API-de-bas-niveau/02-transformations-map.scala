d50c5e18718c84b4a46b5136b2a1403010425b43
rbizoi <razvan.bizoi@gmail.com>
2020-08-12 03:08:47 +0200

    Update 02-transformations-map.scala

--- Chapitre-03/01-Les-API-de-bas-niveau/02-transformations-map.scala
+++ Chapitre-03/01-Les-API-de-bas-niveau/02-transformations-map.scala
@@ -1,28 +1,20 @@
 
 val rdd05 = spark.sparkContext.textFile(
                   "/user/spark/donnees/postesSynop.csv", 8)
-rdd05.take(2)
+rdd05.take(2).foreach(println)
+rdd05.count()
+val rdd06 = rdd05.filter( l => l.substring(0,5).matches("^\\d+$"))
+rdd06.take(2).foreach(println)
+rdd06.count()
+val rdd07 = rdd06.filter( l => l.substring(0,5) < "08000")
+rdd07.take(2).foreach(println)
+rdd07.count()
 
 
 
-
-
-
-
-
-def printConfigs(session: SparkSession) = {
-  val mconf = session.conf.getAll
-  for (k <- mconf.keySet) {
-      if ( k.matches("spark.executor.*") ||
-           k.matches("spark.default.parallelism"))
-              println(s"${k} -> ${mconf(k)}")
-    }
-}
-
-
-
-
-
+rdd01.take(3).foreach(println)
+val rdd08 = rdd01.map(x => ( x._1.toUpperCase,x._2 * 10 ) )
+rdd08.take(3).foreach(println)
 
 case class stationMeteo(station:String,
                         nom:String,
@@ -30,62 +22,154 @@ case class stationMeteo(station:String,
                         longitude:Double,
                         altitude:Int)
 
-
 def transformLignePoste(line: String): stationMeteo = {
   val fields = line.split(';')
   val station: stationMeteo = stationMeteo(
     fields(0),
-    fields(1).toLowerCase.split(' ').map(_.capitalize).mkString(" ").split('-').map(_.capitalize).mkString(" "),
+    fields(1).toLowerCase.split(' ').
+              map(_.capitalize).mkString(" ").
+              split('-').
+              map(_.capitalize).
+              mkString(" "),
     fields(2).toDouble,
     fields(3).toDouble,
     fields(4).toInt)
   return station
 }
 
-val parseVille: (String => String) = (arg: String) => {
-               arg.toLowerCase.split(' ').
-                map(_.capitalize).mkString(" ").
-                split('-').map(_.capitalize).mkString(" ") }
-val sqlfV = udf(parseVille)
+val rdd09 = rdd07.map(transformLignePoste)
+rdd09.take(3).foreach(println)
 
+flatMap(lambda x: range(0, x)
+rdd01.take(3).foreach(println)
 
- val rdd06 = rdd01.map(x => x(0))
-
-
-
- donnees05 = donnees04.filter( lambda ligne :
-                          str(ligne)[0].isdigit() ).\
-                          persist()
-
-donnees11 = donnees10.filter(donnees10.Id < '08000')
-fields(1).substring(0,5),
+rdd10 = rdd01.flatMap(lambda x: range(0, x)
 
 
 
-def transformLignePoste(ligne):
-    champs = ligne.split(";")
-    return ( str(champs[0]),
-            (str(champs[1]).title(),
-            float(champs[2]),
-            float(champs[3]),
-            int(champs[4])))
+val rdd10 = spark.sparkContext.parallelize(Array(1,2,3))
+val rdd11 = rdd10.flatMap(x => x.to(3))
+rdd11.collect().foreach(println)
+
+val rdd12 = spark.sparkContext.parallelize(
+            Array("Heureux qui, comme Ulysse, a fait un beau voyage")).
+                   map(x => x.toLowerCase).
+                   map(x => x.replaceAll("[^\\p{L}]", " ")).
+                   map(x => x.replaceAll("  ", " "))
+val rdd13 = rdd12.flatMap(x => x.split(' '))
+rdd13.collect().foreach(println)
 
 
+val rdd14 = spark.sparkContext.parallelize(
+                                 Array(1,2,3,4,5,6))
+val rdd15 = spark.sparkContext.parallelize(
+                                 Array(6,7,8,4,5,6))
+rdd14.intersection(rdd15).collect().foreach(println)
 
-rdd06 = rdd05.map(transformLignePoste)
-rdd06 = rdd05.map( lambda ligne :
-                   transformLignePoste(ligne)).\
-                   persist()
+val rdd14 = spark.sparkContext.parallelize(
+                                 Array(1,2,3,4,5,6))
+val rdd15 = spark.sparkContext.parallelize(
+                                 Array(6,7,8,4,5,6))
+rdd14.union(rdd15).collect().foreach(println)
 
-donnees07 = donnees06.join(donnees03).persist()
+def transformLignePoste(line: String) = {
+  val champs = line.split(";")
+  val c1     = champs(0)
+  val c2     = champs(1).toLowerCase.split(' ').
+                          map(_.capitalize).mkString(" ").
+                          split('-').
+                          map(_.capitalize).
+                          mkString(" ")
+  val c3     = champs(2).toDouble
+  val c4     = champs(3).toDouble
+  val c5     = champs(4).toInt
+  (c1,(c2,c3,c4,c5))
+}
 
-donnees08 = donnees07.sortByKey().persist()
+val villes = spark.sparkContext.textFile(
+               "/user/spark/donnees/postesSynop.csv").
+               filter( l => l.substring(0,5).matches("^\\d+$")).
+               map(transformLignePoste)
+villes.take(5).foreach(println)
 
-donnees09 = donnees08.map(lambda ligne : tuple([ligne[0]] +
-                           [x for x in ligne[1][0]] +
-                           [x for x in ligne[1][1]]) ).persist()
+def transformLigneMeteo(line: String) = {
+  val champs = line.split(";")
+  val c1 = champs(0)
+  val c2 = champs(7).toDouble - 273.15
+  (c1,c2)
+}
+val meteo = spark.sparkContext.
+       textFile("/user/spark/donnees/meteo/synop.202007.csv").
+       filter(ligne => ligne.substring(0,5).matches("^\\d+$")).
+       map(ligne => ligne.replaceAll("mq","0")).
+       map(transformLigneMeteo)
+meteo.take(5).foreach(println)
+meteo.groupByKey().take(1).foreach(println)
+meteo.groupByKey().reduceByKey( (total,valeur) => total+valeur).take(10).foreach(println)
+meteo.reduceByKey( (total,valeur) => total+valeur).take(10).foreach(println)
 
 
-rdd06 = rdd05.filter( lambda ligne :
-                                                    str(ligne)[0].isdigit() ).\
-                                                    persist()
+def transformLignePoste(line: String) = {
+  val champs = line.split(";")
+  val c1     = champs(0)
+  val c2     = champs(1).toLowerCase.split(' ').
+                          map(_.capitalize).mkString(" ").
+                          split('-').
+                          map(_.capitalize).
+                          mkString(" ")
+  val c3     = champs(2).toDouble
+  val c4     = champs(3).toDouble
+  val c5     = champs(4).toInt
+  (c2,(c1,c3,c4,c5))
+}
+val villes = spark.sparkContext.textFile(
+               "/user/spark/donnees/postesSynop.csv").
+               filter( l => l.substring(0,5).matches("^\\d+$")).
+               map(transformLignePoste)
+villes.take(5).foreach(println)
+villes.sortByKey().take(10).foreach(println)
+villes.map( l =>
+   ((l._2._4,l._1),(l._2._1,l._2._2,l._2._3))).
+   take(3).foreach(println)
+villes.map( l =>
+      ((l._2._4,l._1),(l._2._1,l._2._2,l._2._3))).
+      sortByKey().take(12).foreach(println)
+
+
+def transformLignePoste(line: String) = {
+  val champs = line.split(";")
+  val c1     = champs(0)
+  val c2     = champs(1).toLowerCase.split(' ').
+                          map(_.capitalize).mkString(" ").
+                          split('-').
+                          map(_.capitalize).
+                          mkString(" ")
+  val c3     = champs(2).toDouble
+  val c4     = champs(3).toDouble
+  val c5     = champs(4).toInt
+  (c1,(c2,c3,c4,c5))
+}
+val villes = spark.sparkContext.textFile(
+               "/user/spark/donnees/postesSynop.csv").
+               filter( l => l.substring(0,5).matches("^\\d+$")).
+               map(transformLignePoste)
+villes.take(5).foreach(println)
+
+def transformLigneMeteo(line: String) = {
+  val champs = line.split(";")
+  val c1 = champs(0)
+  val c2 = champs(1).substring(6,8).toInt
+  val c3 = champs(7).toDouble
+  val c4 = champs(20).toInt
+  (c1,(c2,c3,c4))
+}
+
+val meteo = spark.sparkContext.
+       textFile("/user/spark/donnees/meteo/synop.202007.csv").
+       filter(ligne => ligne.substring(0,5).matches("^\\d+$")).
+       map(ligne => ligne.replaceAll("mq","0")).
+       map(transformLigneMeteo)
+meteo.take(5).foreach(println)
+
+
+villes.join(meteo).take(5).foreach(println)
