0844df32c3049fad3463a4fa1b2c828f90eaa8b1
rbizoi <razvan.bizoi@gmail.com>
2020-08-13 12:12:39 +0200

    Create lire-json-opendata.py

---
+++ donnees/lire-json-opendata.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+
+
+url = "https://data.strasbourg.eu/api/records/1.0/search/?dataset=stations-velhop"
+url = "https://data.strasbourg.eu/api/records/1.0/search/?dataset=occupation-parkings-temps-reel"
+url = "https://data.strasbourg.eu/api/records/1.0/search/?dataset=lieux_sport"
+
+
+import json
+import urllib.request
+url = "https://data.strasbourg.eu/api/records/1.0/search/?dataset=stations-velhop"
+response = urllib.request.urlopen(url)
+results = json.loads(response.read().decode())
+lignes = results['nhits']
+url = url+"&q=&lang=fr&rows="+str(lignes)
+response = urllib.request.urlopen(url)
+results = json.loads(response.read().decode())['records']
+print(lignes, [x['fields'] for x in results])
