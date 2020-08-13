4529e7226a959e029fd7b0cb51bcc68ec3577bc4
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 01:25:22 +0200

    Update lire-json-opendata.py

--- donnees/lire-json-opendata.py
+++ donnees/lire-json-opendata.py
@@ -17,3 +17,16 @@ url = url+"&q=&lang=fr&rows="+str(lignes)
 response = urllib.request.urlopen(url)
 results = json.loads(response.read().decode())['records']
 print(lignes, [x['fields'] for x in results])
+
+
+
+import json
+import urllib.request
+url = url = "https://data.strasbourg.eu/api/records/1.0/search/?dataset=occupation-parkings-temps-reel"
+response = urllib.request.urlopen(url)
+results = json.loads(response.read().decode())
+lignes = results['nhits']
+url = url+"&q=&lang=fr&rows="+str(lignes)
+response = urllib.request.urlopen(url)
+results = json.loads(response.read().decode())['records']
+print(lignes, [x['fields'] for x in results])
