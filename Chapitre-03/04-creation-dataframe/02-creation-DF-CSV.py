2ac6024ad601fa0c4cc42e0553a0b29b95c3efb6
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 01:25:47 +0200

    Update 02-creation-DF-CSV.py

---
+++ Chapitre-03/04-creation-dataframe/02-creation-DF-CSV.py
@@ -0,0 +1,36 @@
+from pyspark.sql.types import StructType, \
+     StructField, FloatType, \
+     IntegerType, StringType
+
+meteoDF00  = spark.read.format('csv')         \
+      .option('sep',';')                      \
+      .option('mergeSchema', 'true')          \
+      .option('header','true')                \
+      .option('nullValue','mq')               \
+      .option("inferSchema", "true")           \
+      .load('/user/spark/donnees/meteo.txt')  \
+      .select('numer_sta', 'date', 't',
+              'u', 'vv', 'pres') \
+      .cache()
+
+meteoDF00.printSchema()
+meteoDF00.show(3)
+
+
+schema = StructType([
+        StructField('Id'           , StringType() , True),
+        StructField('ville'        , StringType() , True),
+        StructField('latitude'     , FloatType() , True),
+        StructField('longitude'    , FloatType() , True),
+        StructField('altitude'     , IntegerType() , True)])
+
+postesDF00  = spark.read.format('csv')   \
+      .option('sep',';')                \
+      .option('mergeSchema', 'true')    \
+      .option('header','true')          \
+      .schema(schema)                   \
+      .load('/user/spark/donnees/postesSynop.csv')  \
+      .cache()
+
+meteoDF00.printSchema()
+meteoDF00.select('ville','altitude').show(3)
