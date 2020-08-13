81dda4c6aed624db47236d4ce45e56b8cf616f9e
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 01:25:40 +0200

    Update 03-creation-DF-JSON.scala

--- Chapitre-03/04-creation-dataframe/03-creation-DF-JSON.scala
+++ Chapitre-03/04-creation-dataframe/03-creation-DF-JSON.scala
@@ -1,7 +1,26 @@
-val parkingStras  = spark.read.format("json").
+import org.apache.spark.sql.types._
+val customSchema = StructType(
+   StructField("etat"            , IntegerType , true)::
+   StructField("etat_descriptif" , StringType , true)::
+   StructField("ident"           , StringType, true)::
+   StructField("idsurfs"           , StringType, true)::
+   StructField("infousager"      , StringType, true)::
+   StructField("libre"           , IntegerType, true)::
+   StructField("nom_parking"     , StringType, true)::
+   StructField("total"           , IntegerType  , true)::Nil)
+
+val parkingStras01  = spark.read.format("json").
       option("mergeSchema", "true").
       option("inferSchema", "true").
-      load("/user/spark/donnees/parking_stras.json").
+      load("/user/spark/donnees/json/parking_stras.json").
       cache()
 
-parkingStras.printSchema()
+parkingStras01.printSchema()
+
+val parkingStras02  = spark.read.format("json").
+      option("mergeSchema", "true").
+      schema(customSchema).
+      load("/user/spark/donnees/json/parking_stras.json").
+      cache()
+
+parkingStras02.printSchema()
