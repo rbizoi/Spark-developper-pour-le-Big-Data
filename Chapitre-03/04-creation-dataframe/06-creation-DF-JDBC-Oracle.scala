41f4193587c284830905af54867ac0475187b9f5
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 19:02:36 +0200

    Update 06-creation-DF-JDBC-Oracle.scala

--- Chapitre-03/04-creation-dataframe/06-creation-DF-JDBC-Oracle.scala
+++ Chapitre-03/04-creation-dataframe/06-creation-DF-JDBC-Oracle.scala
@@ -1,26 +1,32 @@
-pyspark \
-    --master spark://jupiter.olimp.fr:7077 \
-    --executor-cores 8 \
-    --executor-memory 20g \
-    --packages mysql:mysql-connector-java:8.0.20
 
-url  = "jdbc:mysql://jupiter.olimp.fr:3306/cours?serverTimezone=UTC#"
-user        = "spark"
-password    = "CoursSPARK3#"
+from pyspark.sql import SparkSession
 
-donnees00 = spark.read \
-        .format("jdbc") \
-        .option("url", url) \
-        .option("dbtable", 'categories') \
-        .option("user", user) \
-        .option("password", password).load()
-donnees00.show(3)
+val spark = SparkSession.builder\
+          .config("spark.jars.packages",
+                         "io.delta:delta-core_2.12:0.7.0") \
+          .config("spark.sql.extensions",
+                         "io.delta.sql.DeltaSparkSessionExtension")\
+          .getOrCreate()
 
-requette    = "select titre,nom,prenom from employes"
-donnees01 = spark.read \
+val url = "jdbc:oracle:thin:@(DESCRIPTION =(ADDRESS_LIST =(ADDRESS =(PROTOCOL=TCP)(HOST=192.168.1.25)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=pdbcours1)))"
+val user = "stagiaire"
+val password = "CoursSPARK3#"
+
+val donnees00 = spark.read.
+                      format("jdbc").
+                      option("url", url).
+                      option("dbtable", "categories").
+                      option("user", user).
+                      option("password", password).
+                      load()
+
+val donnees00.show(3)
+
+val requette    = "select titre,nom,prenom from employes"
+val donnees01 = spark.read \
         .format("jdbc") \
         .option("url", url) \
         .option("query", requette) \
         .option("user", user) \
         .option("password", password).load()
-donnees01.show(3)
+val donnees01.show(3)
