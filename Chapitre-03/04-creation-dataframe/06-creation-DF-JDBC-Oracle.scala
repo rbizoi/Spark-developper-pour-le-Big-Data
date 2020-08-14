388560b94b647e00c049a244de7094734da308b0
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 20:26:01 +0200

    Update 06-creation-DF-JDBC-Oracle.scala

--- Chapitre-03/04-creation-dataframe/06-creation-DF-JDBC-Oracle.scala
+++ Chapitre-03/04-creation-dataframe/06-creation-DF-JDBC-Oracle.scala
@@ -1,16 +1,32 @@
 
-from pyspark.sql import SparkSession
+import org.apache.spark.sql.SparkSession
 
-val spark = SparkSession.builder\
-          .config("spark.jars.packages",
-                         "io.delta:delta-core_2.12:0.7.0") \
-          .config("spark.sql.extensions",
-                         "io.delta.sql.DeltaSparkSessionExtension")\
-          .getOrCreate()
+val spark = SparkSession.builder.
+          config("spark.jars.packages",
+                         "io.delta:delta-core_2.12:0.7.0").
+          config("spark.sql.extensions",
+                         "io.delta.sql.DeltaSparkSessionExtension").
+          getOrCreate()
 
-val url = "jdbc:oracle:thin:@(DESCRIPTION =(ADDRESS_LIST =(ADDRESS =(PROTOCOL=TCP)(HOST=192.168.1.25)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=pdbcours1)))"
+val url = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.1.25)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=pdbcours1)))"
 val user = "stagiaire"
 val password = "CoursSPARK3#"
+val format     = "delta"
+val repertoire = "donnees/delta/"
+
+val donnees = spark.read.format("delta").load(
+                "donnees/delta/FOURNISSEURS_delta").cache()
+                
+donnees.write.format("jdbc").
+        option("url", url).
+        option("dbtable", "FOURNISSEURS").
+        option("user", user).
+        option("password", password).
+        save()
+
+
+
+
 
 val donnees00 = spark.read.
                       format("jdbc").
@@ -20,7 +36,7 @@ val donnees00 = spark.read.
                       option("password", password).
                       load()
 
-val donnees00.show(3)
+donnees00.show(3)
 
 val requette    = "select titre,nom,prenom from employes"
 val donnees01 = spark.read \
