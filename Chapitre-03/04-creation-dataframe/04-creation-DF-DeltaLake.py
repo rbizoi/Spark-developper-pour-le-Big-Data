f45c22fe368325d75919628e5b0c5f40d20bdcd6
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 16:40:34 +0200

    Update 04-creation-DF-DeltaLake.py

--- Chapitre-03/04-creation-dataframe/04-creation-DF-DeltaLake.py
+++ Chapitre-03/04-creation-dataframe/04-creation-DF-DeltaLake.py
@@ -4,33 +4,28 @@ pyspark \
     --executor-memory 20g \
     --packages io.delta:delta-core_2.12:0.7.0
 
-fichier = 'donnees/etl/stagiaire/Oracle/EMPLOYES.parquet'
-donnees = spark.read.parquet(fichier)
-donnees.select("titre","nom","prenom",).show(10,False)
 
-donnees.write.mode("overwrite").format('delta').save('donnees/etl/stagiaire/Oracle/EMPLOYES.delta')
-donnees01 = spark.read.format('delta').load('donnees/etl/stagiaire/Oracle/EMPLOYES.delta')
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder\
+          .config("spark.jars.packages",
+                         "io.delta:delta-core_2.12:0.7.0") \
+          .config("spark.sql.extensions",
+                         "io.delta.sql.DeltaSparkSessionExtension")\
+          .getOrCreate()
 
+repertoire = 'donnees/parquet/'
+format     = '.parquet'
+fichiers   = ['ACHETEURS','ADRESSES','AGENCES','CATEGORIES',
+    'CLIENTS','COMMANDES','COMMISSIONNEMENTS',
+    'COMMISSIONNEMENTS_AGENCES','COMMISSIONNEMENTS_VENDEURS',
+    'COORDONEES','DETAILS_COMMANDES','EMPLOYES','FACTURES',
+    'FOURNISSEURS','GESTIONS_STOCKS','MAGASINS',
+    'MOUVEMENTS','PRODUITS','RELANCES','STOCKS_ENTREPOTS',
+    'TVA_PRODUIT','VENDEURS','VILLES']
 
-url  = "jdbc:mysql://jupiter.olimp.fr:3306/cours?serverTimezone=UTC#"
-user        = "spark"
-password    = "CoursSPARK3#"
-format     = "parquet"
-
-donnees00 = spark.read \
-        .format("jdbc") \
-        .option("url", url) \
-        .option("dbtable", 'categories') \
-        .option("user", user) \
-        .option("password", password).load()
-donnees00.show(3)
-
-requette    = "select titre,nom,prenom from employes"
-donnees01 = spark.read \
-        .format("jdbc") \
-        .option("url", url) \
-        .option("query", requette) \
-        .option("user", user) \
-        .option("password", password).load()
-donnees01.show(3)
+for nom in fichiers :
+    donnees = spark.read.format('parquet'
+                         ).load(repertoire+nom+'.parquet').cache()
+    donnees.write.mode('overwrite').format('delta'
+                         ).save('donnees/delta/'+nom+'_delta')
