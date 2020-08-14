ca1c29869b3f6bb03c7fc15be9401436b2e840ec
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 20:59:11 +0200

    Update 03-creation-DF-Parquet-ecriture.scala

--- Chapitre-03/04-creation-dataframe/03-creation-DF-Parquet-ecriture.scala
+++ Chapitre-03/04-creation-dataframe/03-creation-DF-Parquet-ecriture.scala
@@ -4,10 +4,9 @@ val donneesMeteo  = spark.read.format("csv").
       option("header","true").
       option("nullValue","mq").
       option("inferSchema", "true").
-      load("/user/spark/donnees/meteo.txt").
-      cache()
+      load("/user/spark/donnees/meteo.txt")
 
-fichier = "/user/spark/donnees/json/parking_stras_json"
-format  = "json"
+val fichier = "/user/spark/donnees/meteo_parquet"
+val format  = "parquet"
 donneesMeteo.write.mode("overwrite").format(format).save(fichier)
 :q
