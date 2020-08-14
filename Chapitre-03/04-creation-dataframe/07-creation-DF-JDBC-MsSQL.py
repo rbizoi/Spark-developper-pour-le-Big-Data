cb0d42baf9406241d0cc1c844dcf538f486ac6d1
rbizoi <razvan.bizoi@gmail.com>
2020-08-14 20:59:06 +0200

    Update 07-creation-DF-JDBC-MsSQL.py

--- Chapitre-03/04-creation-dataframe/07-creation-DF-JDBC-MsSQL.py
+++ Chapitre-03/04-creation-dataframe/07-creation-DF-JDBC-MsSQL.py
@@ -2,32 +2,35 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder\
           .config('spark.jars.packages',
-                         'mysql:mysql-connector-java:8.0.20') \
-          .config('spark.jars.packages',
                          'io.delta:delta-core_2.12:0.8.0') \
           .config('spark.sql.extensions',
                          'io.delta.sql.DeltaSparkSessionExtension')\
           .getOrCreate()
 
+# ---------------------------------------------------------------------------------------------------------------------
+servername = 'jdbc:sqlserver://192.168.1.25'
+dbname     = 'cours'
+url        = servername + ';' + 'databaseName=' + dbname + ';'
+format     = 'com.microsoft.sqlserver.jdbc.spark'
+# ---------------------------------------------------------------------------------------------------------------------
+user       = 'sa'
+password   = 'CoursNFP107!'
+# ---------------------------------------------------------------------------------------------------------------------
 
-
-url  = "jdbc:mysql://jupiter.olimp.fr:3306/cours?serverTimezone=UTC#"
-user        = "spark"
-password    = "CoursSPARK3#"
 
 donnees00 = spark.read \
-        .format("jdbc") \
-        .option("url", url) \
-        .option("dbtable", 'categories') \
-        .option("user", user) \
-        .option("password", password).load()
+        .format(format) \
+        .option('url', url) \
+        .option('dbtable', 'categories') \
+        .option('user', user) \
+        .option('password', password).load()
 donnees00.show(3)
 
-requette    = "select titre,nom,prenom from employes"
+requette    = 'select titre,nom,prenom from employes'
 donnees01 = spark.read \
-        .format("jdbc") \
-        .option("url", url) \
-        .option("query", requette) \
-        .option("user", user) \
-        .option("password", password).load()
+        .format('jdbc') \
+        .option('url', url) \
+        .option('query', requette) \
+        .option('user', user) \
+        .option('password', password).load()
 donnees01.show(3)
