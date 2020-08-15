9b5336ae91a1d8ed27a41d31a951c8f62ae9f710
rbizoi <razvan.bizoi@gmail.com>
2020-08-15 21:16:34 +0200

    Update 08-creation-DF-fichier-binnaire.py

--- Chapitre-03/04-creation-dataframe/08-creation-DF-fichier-binnaire.py
+++ Chapitre-03/04-creation-dataframe/08-creation-DF-fichier-binnaire.py
@@ -1,1 +1,9 @@
 donnees = spark.read.format('binaryFile').option('pathGlobFilter', '*.jpg').load('donnees/images')
+donnees.printSchema()
+donnees.select('length','path','modificationTime').show(3)
+
+from pyspark.ml import image
+donnees = spark.read.format('image').load('donnees/images')
+donnees.printSchema()
+donnees.select('image.height','image.width',
+               'image.nChannels','image.mode').show(3)
