306d5299569139b9dc9da95607e6c8a96eea0553
rbizoi <razvan.bizoi@gmail.com>
2020-08-15 22:33:34 +0200

    Update 06-creation-DF-JDBC-Oracle.scala

--- Chapitre-03/04-creation-dataframe/06-creation-DF-JDBC-Oracle.scala
+++ Chapitre-03/04-creation-dataframe/06-creation-DF-JDBC-Oracle.scala
@@ -1,12 +1,19 @@
+//spark-shell \
+//    --master spark://jupiter.olimp.fr:7077 \
+//    --executor-cores 8 \
+//    --executor-memory 20g \
+//    --jars /usr/share/spark/jars/ojdbc8.jar
 
 import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder.
-          config("spark.jars.packages",
-                         "io.delta:delta-core_2.12:0.8.0").
-          config("spark.sql.extensions",
-                         "io.delta.sql.DeltaSparkSessionExtension").
-          getOrCreate()
+              config("spark.jars.packages",
+                             "oracle:ojdbc8").
+              config("spark.jars.packages",
+                             "io.delta:delta-core_2.12:0.8.0").
+              config("spark.sql.extensions",
+                             "io.delta.sql.DeltaSparkSessionExtension").
+              getOrCreate()
 
 val url = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.1.25)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=pdbcours1)))"
 val user = "stagiaire"
