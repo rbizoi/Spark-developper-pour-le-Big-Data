ba691154bc5b2ac4ca8ab3ae36c629dfe197b397
rbizoi <razvan.bizoi@gmail.com>
2020-08-18 14:14:36 +0200

    Create 01-les-transformations.scala

---
+++ Module-03/03-architecture-aplication/01-les-transformations.scala
@@ -0,0 +1,10 @@
+var lines = spark.sparkContext.textFile("donnees/livres")
+val transfLines = lines.map(x => x.toLowerCase).
+         map(x => x.replaceAll("[^\\p{L}]", " "))
+val mots = transfLines.flatMap(x => x.split(" "))
+val motsAvec1 = mots.map(t => (t, 1))
+val motsOccurences = motsAvec1.reduceByKey((a, b) => a + b)
+val motsFiltres = motsOccurences.filter(w => w._1.length() > 3)
+val occurecesMots = motsFiltres.map( x => (x._2,x._1))
+val occurecesOrdonnees = occurecesMots.sortByKey(ascending = false)
+val sortie = occurecesOrdonnees.collect()
