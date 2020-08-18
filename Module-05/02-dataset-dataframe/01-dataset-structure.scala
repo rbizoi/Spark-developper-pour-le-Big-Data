844e1f70f92acd07d8f349115343e720e3612b18
rbizoi <razvan.bizoi@gmail.com>
2020-08-18 10:33:33 +0200

    Create 01-dataset-structure.scala

---
+++ Module-05/02-dataset-dataframe/01-dataset-structure.scala
@@ -0,0 +1,42 @@
+case class typePosteSynop(
+                           Altitude:Long ,
+                           ID:String,
+                           Latitude:Double,
+                           Longitude:Double,
+                           Nom:String
+                         )
+
+val stationsDF00  =  spark.read.json("donnees/json/postes-synop.json").as[typePosteSynop]
+stationsDF00.printSchema()
+
+val stationsDF01 = stationsDF00.map(l =>
+                  typePosteSynop(  l.Altitude ,
+                                    l.ID,
+                                    l.Latitude,
+                                    l.Longitude,
+                                    l.Nom.toLowerCase.split(' ').
+                                      map(_.capitalize).mkString(" ").
+                                      split('-').
+                                      map(_.capitalize).
+                                      mkString(" ") )
+                                    ).as[typePosteSynop]
+case class typeRPosteSynop(
+                            Nom:String,
+                            Altitude:Long ,
+                            Longitude:Double
+                         )
+stationsDF01.map(l =>
+                  typeRPosteSynop( l.Nom,
+                                   l.Altitude ,
+                                   l.Longitude)).
+             as[typeRPosteSynop].
+             filter( l => {l.Altitude > 400 && l.Longitude > 3 }).show()
+
+stationsDF01.select( $"Nom", $"Altitude", $"Longitude").
+        where("Altitude > 400").
+        where("Longitude > 3").show()
+
+val stationsDF02 = stationsDF01.toDF()
+stationsDF02.select( $"Nom", $"Altitude", $"Longitude").
+        where("Altitude > 400").
+        where("Longitude > 3").show()
