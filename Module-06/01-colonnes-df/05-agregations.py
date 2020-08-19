89141fe6417754b2fd79d2030b23c6ec7c5682b3
rbizoi <razvan.bizoi@gmail.com>
2020-08-19 22:58:55 +0200

    Update 05-agregations.py

--- Module-06/01-colonnes-df/05-agregations.py
+++ Module-06/01-colonnes-df/05-agregations.py
@@ -1,5 +1,10 @@
 from pyspark.sql.functions import *
 
+
+meteo.where('id < 8000')\
+     .select('annee','mois_jour','temperature','precipitations')\
+     .describe().show()
+
 meteo.where('id < 8000')\
      .select('annee','mois_jour','temperature')\
      .describe().show()
@@ -20,7 +25,6 @@ meteo.where('id < 8000')\
      .groupBy('id','annee')\
      .max('temperature','visibilite','pression').show(5)
 
-
 meteo.where('id < 8000')\
      .groupBy('id','annee')\
      .agg(
@@ -28,8 +32,80 @@ meteo.where('id < 8000')\
             round(avg('temperature'),2).alias('temperature'),
             round(avg('humidite'),2).alias('humidite'),
             round(avg('visibilite'),2).alias('visibilite'),
-            round(avg('pression'),2).alias('pression')
-     ).show(10)
+            round(avg('pression'),2).alias('pression'),
+            round(sum('pression')).alias('precipitations'))\
+     .orderBy("id","annee")\
+     .show(28)
+
+meteo.where('id < 8000')\
+     .groupBy('annee','mois')\
+     .agg(round(sum('pression') / 1000).alias('precipitations'))\
+     .orderBy('annee','mois')\
+     .show(5)
+
+meteo.where('id < 8000')\
+     .groupBy('annee','mois')\
+     .agg(round(sum('pression') / 1000).alias('precipitations'))\
+     .rollup('annee','mois')\
+     .agg( round(sum('precipitations')).alias('precipitations'))\
+     .orderBy(col('annee').asc_nulls_last(),
+              col('mois').asc_nulls_last())\
+     .show(14)
+
+
+meteo.where('id < 8000')\
+     .groupBy('annee','mois')\
+     .agg(round(sum('pression') / 1000).alias('precipitations'))\
+     .count()
+
+meteo.where('id < 8000')\
+     .groupBy('annee','mois')\
+     .agg(round(sum('pression') / 1000).alias('precipitations'))\
+     .rollup('annee','mois')\
+     .agg( round(sum('precipitations')).alias('precipitations'))\
+     .count()
+
+meteo.where('id < 8000')\
+     .groupBy('annee','mois')\
+     .agg(round(sum('pression') / 1000).alias('precipitations'))\
+     .cube('annee','mois')\
+     .agg( round(sum('precipitations')).alias('precipitations'))\
+     .count()
+
+meteo.where('id < 8000')\
+     .groupBy('annee','mois')\
+     .agg(round(sum('pression') / 1000).alias('precipitations'))\
+     .cube('annee','mois')\
+     .agg( round(sum('precipitations')).alias('precipitations'))\
+     .orderBy(col('annee'),col('mois'))\
+     .show(24)
+
+meteo.where('id < 8000')\
+     .groupBy('annee','mois')\
+     .agg(round(sum('pression') / 1000).alias('precipitations'))\
+     .rollup('annee','mois')\
+     .agg(round(sum('precipitations')).alias('precipitations'))\
+     .show(2000)
+
+meteo.where('id < 8000')\
+     .rollup('annee','mois')\
+     .agg(
+            round(sum('precipitations')).alias('precipitations'))\
+     .orderBy(col('annee').asc_nulls_last(),
+              col('mois').asc_nulls_last())\
+     .show(16)
+
+meteo.where('id < 8000')\
+     .rollup('annee','mois')\
+     .agg(
+            round(avg('temperature'),2).alias('temperature'),
+            round(avg('humidite'),2).alias('humidite'),
+            round(avg('visibilite'),2).alias('visibilite'),
+            round(avg('pression'),2).alias('pression'),
+            round(avg('pression'),2).alias('precipitations'))\
+     .orderBy(col('annee').asc_nulls_last(),
+              col('mois').asc_nulls_last())\
+     .show(16)
 
 meteo.where('id < 8000')\
      .groupBy('id','annee')\
@@ -40,13 +116,11 @@ meteo.where('id < 8000')\
      ).toDF('id','annee','humidite','temperature','nb_villes').show(10)
 
 
-
 meteo.where('id < 8000 and annee > 2014')\
      .groupBy('id','annee')\
      .agg( round(avg('temperature'),2).alias('temperature'))\
      .orderBy("id","annee")\
      .show(10)
-
 
 meteo.where('id < 8000 and annee > 2014')\
       .groupBy('id')\
