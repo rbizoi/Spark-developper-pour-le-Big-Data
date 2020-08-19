b9755f08b0bd241e71ef0772aad13a3c802c711a
rbizoi <razvan.bizoi@gmail.com>
2020-08-19 15:15:32 +0200

    Create 00-init-dataframes-module06.py

---
+++ Module-06/01-colonnes-df/00-init-dataframes-module06.py
@@ -0,0 +1,48 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types     import StructType, \
+     StructField, FloatType, \
+     IntegerType, StringType
+
+meteoDataFrame  = spark.read.format('csv')\
+    .option('sep',';')\
+    .option('header','true')\
+    .option('nullValue','mq')\
+    .option('inferSchema', 'true')\
+    .load('donnees/meteo')\
+    .cache()
+
+meteoDataFrame.columns
+meteoDataFrame.printSchema()
+
+schema = StructType([
+        StructField('Id'           , StringType() , True),
+        StructField('ville'        , StringType() , True),
+        StructField('latitude'     , FloatType() , True),
+        StructField('longitude'    , FloatType() , True),
+        StructField('altitude'     , IntegerType() , True)])
+
+villes  = spark.read.format('csv')   \
+      .option('sep',';')                \
+      .option('mergeSchema', 'true')    \
+      .option('header','true')          \
+      .schema(schema)                   \
+      .load('/user/spark/donnees/postesSynop.csv')  \
+      .cache()
+
+
+meteo = meteoDataFrame.select(
+                 col('numer_sta'),
+                 col('date').cast("string")[0:4].cast('int') ,
+                 col('date').cast("string")[5:2].cast('int'),
+                 col('date').cast("string")[7:2].cast('int'),
+                 col('date').cast("string")[5:4],
+                 col('t') - 273.15,
+                 col('u') / 100 ,
+                 col('vv') / 1000 ,
+                 col('pres') / 1000
+                 ).toDF('id','annee','mois','jour','mois_jour',
+                        'temperature','humidite','visibilite','pression')\
+             .cache()
+             
+meteo.select('annee','mois','jour','temperature','humidite',
+             'visibilite','pression').show(3)
