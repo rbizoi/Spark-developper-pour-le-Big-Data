bbea53f757f6cae052727a1f47d5d0c676cafcfb
rbizoi <razvan.bizoi@gmail.com>
2020-08-19 10:07:39 +0200

    Update 02-lire-meteo.py

--- Module-06/01-colonnes-df/02-lire-meteo.py
+++ Module-06/01-colonnes-df/02-lire-meteo.py
@@ -1,7 +1,21 @@
-meteoDataFrame  = spark.read.format("csv")\
-    .option("sep",";")\
-    .option("header","true")\
-    .option("nullValue","mq")\
-    .option("inferSchema", "true")\
-    .load("donnees/meteo")
-meteoDataFrame.select("numer_sta","date","t").show(3)
+from pyspark.sql.functions import expr,col
+
+meteoDataFrame  = spark.read.format('csv')\
+    .option('sep',';')\
+    .option('header','true')\
+    .option('nullValue','mq')\
+    .option('inferSchema', 'true')\
+    .load('donnees/meteo')\
+    .cache()
+
+meteoDataFrame.select('numer_sta',"date","t").show(3)
+meteoDataFrame.numer_sta
+meteoDataFrame["numer_sta"]
+meteoDataFrame.t
+meteoDataFrame.select('numer_sta',meteoDataFrame.t - 273.15).show(3)
+meteoDataFrame.select('numer_sta',col('t') - 273.15).show(3)
+
+meteoDataFrame.select('numer_sta',
+        expr('t  - 273.15').alias('temperature'),
+        expr('(t + pres/1000)*vv/1000')
+             .alias('calc')).show(3)
