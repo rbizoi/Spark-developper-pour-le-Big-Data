ea5e349305b66645514f78464ccc82e25fbf2918
rbizoi <razvan.bizoi@gmail.com>
2020-08-19 17:20:24 +0200

    Update 05-agregations.scala

--- Module-06/01-colonnes-df/05-agregations.scala
+++ Module-06/01-colonnes-df/05-agregations.scala
@@ -1,30 +1,33 @@
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
-
-meteoDataFrame.columns
-meteoDataFrame.printSchema()
-
-meteoDataFrame.select('numer_sta,
-        expr("t  - 273.15").alias("temperature"),
-        expr("(t + pres/1000)*vv/1000")
-             .alias("calc")).show(3)
-
-meteoDataFrame.selectExpr("*"," t  - 273.15 as temperature")).show(3)
-
-villes.select('Id,'ville,'latitude,
-              'longitude,'altitude,
-              expr("altitude * 1000").alias("alt")).show(3)
-
-villes.selectExpr("*","altitude * 1000 as alt").show(3)
-
-villes.drop("Id", "latitude", "longitude").show(3)
-
-
-
-meteo.select("annee","mois","jour","temperature","humidite",
-                          "visibilite","pression").show(3)
 
 meteo.where("id < 8000").
      select("annee","mois_jour",
              "temperature").
      describe().show()
+
+meteo.where("id < 8000").count()
+meteo.count()
+
+meteo.where("id < 8000").
+     select("humidite","visibilite","pression").
+     describe().show()
+
+
+meteo.where("id < 8000").
+          groupBy("id","annee").
+          agg(
+                 count("id").alias("nb_villes"),
+                 round(avg("temperature"),2).alias("temperature"),
+                 round(avg("humidite"),2).alias("humidite"),
+                 round(avg("visibilite"),2).alias("visibilite"),
+                 round(avg("pression"),2).alias("pression")
+          ).show(10)
+
+meteo.where("id < 8000").
+               groupBy("id","annee").
+               agg(
+                      "id"->"count",
+                      "temperature"->"avg",
+                      "humidite"->"avg"
+               ).toDF("id","annee","nb_villes","temperature",
+                      "humidite").show(10)
