f8e4ef9682b34e2f74307977d322b7fd1fd2602f
rbizoi <razvan.bizoi@gmail.com>
2020-08-20 00:51:35 +0200

    Update 06-jointures.scala

--- Module-06/01-colonnes-df/06-jointures.scala
+++ Module-06/01-colonnes-df/06-jointures.scala
@@ -22,3 +22,20 @@ meteo.join(villes.withColumnRenamed("Id", "id"),"id").
       select("ville","annee","mois_jour",
               "temperature","precipitations").
       show(10)
+
+
+dfa.show()
+dfb.show()
+
+dfa.join(dfb,"ville").show()
+dfa.join(dfb,dfa.col("ville") === dfb.col("ville"),"inner").show()
+dfa.join(dfb,dfa.col("ville") === dfb.col("ville"),"outer").show()
+
+dfa.join(dfb,dfa.col("ville") === dfb.col("ville"),"left").show()
+dfa.join(dfb,dfa.col("ville") === dfb.col("ville"),"right").show()
+
+dfa.join(dfb,dfa.col("ville") === dfb.col("ville"),"full").show()
+
+
+dfa.join(dfb,dfa.col("ville") === dfb.col("ville"),"left_semi").show()
+dfa.join(dfb,dfa.col("ville") === dfb.col("ville"),"left_anti").show()
