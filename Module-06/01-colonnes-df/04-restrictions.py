9dd23d7dc0195766cef1d86b1fda52fb8cc32404
rbizoi <razvan.bizoi@gmail.com>
2020-08-19 15:15:42 +0200

    Update 04-restrictions.py

--- Module-06/01-colonnes-df/04-restrictions.py
+++ Module-06/01-colonnes-df/04-restrictions.py
@@ -1,40 +1,24 @@
-from pyspark.sql.functions import expr,col
-from pyspark.sql.types     import StructType, \
-     StructField, FloatType, \
-     IntegerType, StringType
+from pyspark.sql.functions import *
 
-meteoDataFrame  = spark.read.format('csv')\
-    .option('sep',';')\
-    .option('header','true')\
-    .option('nullValue','mq')\
-    .option('inferSchema', 'true')\
-    .load('donnees/meteo')\
-    .cache()
+meteoDataFrame.count()
+meteoDataFrame.selectExpr(
+        'numer_sta','t  - 273.15 as temperature'
+        ).limit(5).show()
 
-meteoDataFrame.select('numer_sta',
-        expr('t  - 273.15').alias('temperature'),
-        expr('(t + pres/1000)*vv/1000')
-             .alias('calc')).show(3)
+meteoDataFrame.select('numer_sta').count()
+meteoDataFrame.select('numer_sta').distinct().count()
+meteoDataFrame.select('numer_sta').distinct().show(3)
+meteoDataFrame.distinct().count()
 
-schema = StructType([
-        StructField('Id'           , StringType() , True),
-        StructField('ville'        , StringType() , True),
-        StructField('latitude'     , FloatType() , True),
-        StructField('longitude'    , FloatType() , True),
-        StructField('altitude'     , IntegerType() , True)])
+villes.where('latitude > 49').show()
+villes.where('latitude > 49 and altitude > 90').show()
+villes.where('latitude > 49 or altitude > 800').show()
 
-villes  = spark.read.format('csv')   \
-      .option('sep',';')                \
-      .option('mergeSchema', 'true')    \
-      .option('header','true')          \
-      .schema(schema)                   \
-      .load('/user/spark/donnees/postesSynop.csv')  \
-      .cache()
+meteoDataFrame.count()
+meteoDataFrame.sample(1/100).count()
+meteoDataFrame.sample(True,1/100).count()
 
-villes.select('*',expr('altitude * 1000').alias('alt')).show(3)
-villes.selectExpr('*','altitude * 1000 as alt').show(3)
+villes.sample(True,2/100,0).show()
+villes.filter("Id == '07168' or Id == '07280'").show()
 
-meteoDataFrame.selectExpr('numer_sta',
-        expr('t  - 273.15').alias('temperature'),
-        expr('(t + pres/1000)*vv/1000')
-             .alias('calc')).show(3)
+meteoDataFrame.groupBy('numer_sta').show(3)
