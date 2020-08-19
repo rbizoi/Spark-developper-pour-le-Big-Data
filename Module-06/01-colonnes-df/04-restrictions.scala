d0f5a48eec4e72c616c4109f80632ca9a7cb5b48
rbizoi <razvan.bizoi@gmail.com>
2020-08-19 15:15:45 +0200

    Update 04-restrictions.scala

--- Module-06/01-colonnes-df/04-restrictions.scala
+++ Module-06/01-colonnes-df/04-restrictions.scala
@@ -1,42 +1,20 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
-val meteoDataFrame  = spark.read.format("csv").
-    option("sep",";").
-    option("header","true").
-    option("nullValue","mq").
-    option("inferSchema", "true").
-    load("donnees/meteo").
-    cache()
-meteoDataFrame.columns
-meteoDataFrame.printSchema()
+meteoDataFrame.count()
+meteoDataFrame.selectExpr(
+       "numer_sta","t  - 273.15 as temperature"
+       ).limit(5).show()
 
-meteoDataFrame.select('numer_sta,
-        expr("t  - 273.15").alias("temperature"),
-        expr("(t + pres/1000)*vv/1000")
-             .alias("calc")).show(3)
+meteoDataFrame.select("numer_sta").count()
+meteoDataFrame.select("numer_sta").distinct.count()
+meteoDataFrame.select("numer_sta").distinct.show(3)
 
-meteoDataFrame.selectExpr("*"," t  - 273.15 as temperature")).show(3)
+villes.where('latitude > 49).show()
 
-val schema = StructType(
-   StructField("Id"        , StringType , true)::
-   StructField("ville"     , StringType , true)::
-   StructField("latitude"  , FloatType  , true)::
-   StructField("longitude" , FloatType  , true)::
-   StructField("altitude"  , IntegerType, true)::Nil)
+villes.where("latitude > 49" and "altitude > 90").show()
+villes.where("latitude > 49" or "altitude > 90").show()
 
-val villes  = spark.read.format("csv").
-             option("sep",";").
-             option("mergeSchema", "true").
-             option("header","true").
-             schema(schema).
-             load("/user/spark/donnees/postesSynop.csv").
-             cache()
+villes.sample(0.02,0).show()
 
-villes.select('Id,'ville,'latitude,
-              'longitude,'altitude,
-              expr("altitude * 1000").alias("alt")).show(3)
-
-villes.selectExpr("*","altitude * 1000 as alt").show(3)
-
-villes.drop("Id", "latitude", "longitude").show(3)
+villes.filter("Id == '07168' or Id == '07280'").show()
