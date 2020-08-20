a8d5db16fad44225e7b3468fbbb9258ba62aeb02
rbizoi <razvan.bizoi@gmail.com>
2020-08-20 10:53:45 +0200

    Create 01-restrictions.scala

---
+++ Module-06/03-restrictions/01-restrictions.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+meteoDataFrame.count()
+meteoDataFrame.selectExpr(
+       "numer_sta","t  - 273.15 as temperature"
+       ).limit(5).show()
+
+meteoDataFrame.select("numer_sta").count()
+meteoDataFrame.select("numer_sta").distinct.count()
+meteoDataFrame.select("numer_sta").distinct.show(3)
+
+villes.where('latitude > 49).show()
+
+villes.where("latitude > 49" and "altitude > 90").show()
+villes.where("latitude > 49" or "altitude > 90").show()
+
+villes.sample(0.02,0).show()
+
+villes.filter("Id == '07168' or Id == '07280'").show()
