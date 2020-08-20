09fa061811e724fbed12e46a9c100e7affa0d686
rbizoi <razvan.bizoi@gmail.com>
2020-08-20 22:27:05 +0200

    Create 01-fonctions-integrees-dates.py

---
+++ Module-06/08-fonctions-integrees/01-fonctions-integrees-dates.py
@@ -0,0 +1,84 @@
+from pyspark.sql.functions import *
+
+meteoDataFrame.columns
+
+df01 = meteoDataFrame.select('date')\
+         .withColumn('d00',col('date')[0:4])\
+         .withColumn('d01',to_date(col('date')[0:8],'yyyyMMdd'))\
+         .withColumn('d02',to_timestamp(col('date').cast('string'),'yyyyMMddHHmmss'))
+df01.show(10)
+df01.printSchema()
+
+unix_timestamp
+
+
+
+meteo = meteoDataFrame.select(
+         col('numer_sta'),
+         to_date(col('date')[0:8],'yyyyMMdd'),
+         to_timestamp(col('date').cast('string'),'yyyyMMddHHmmss'),
+         col('date')[0:4].cast('int') ,
+         col('date')[5:2].cast('int'),
+         col('date')[7:2].cast('int'),
+         col('date')[5:4],
+         round(col('t') - 273.15,2),
+         col('u') / 100 ,
+         col('vv') / 1000 ,
+         col('pres') / 1000,
+         col('rr1'))\
+     .toDF('id','date','timestamp','annee','mois','jour','mois_jour',
+           'temperature','humidite','visibilite','pression','precipitations')\
+     .cache()
+
+meteo.select('date','timestamp','temperature','humidite',
+             'visibilite','pression').show(3)
+
+meteo.select(date_format('date','d dd DDD M MMM MMMM yy yyyy')).show(3,truncate=False)
+
+meteo.sample(0.3)\
+     .orderBy(desc('timestamp'))\
+     .select(date_format('timestamp','h H m s S'))\
+     .show(10,truncate=False)
+
+meteo.sample(0.3)\
+     .orderBy(desc('timestamp'))\
+     .select(date_format('date','q Q E F'))\
+     .show(3,truncate=False)
+
+
+meteo.select('date',
+              date_add('date',1).alias('demain'),
+              date_add('date',-1).alias('hier'),
+              add_months('date',1).alias('mois dernier'),
+              add_months('date',-1).alias('mois prochain'),
+              )\
+     .show(1)
+
+
+meteo.select('date',
+              next_day  ('date',"Wed").alias("prochain mercredi"),
+              last_day  ('date').alias("dernier jour du mois"),
+            ).show(1)
+
+
+
+meteo.select('date',
+              current_date().alias("date du jour"),
+              datediff(current_date(),'date').alias("jours"),
+              months_between(current_date(),'date').alias("mois"),
+              months_between(current_date(),
+                             date_add('date',19)).alias("mois"),
+            ).show(1)
+
+
+meteo.orderBy(desc('timestamp'))\
+      .select('timestamp',
+              date_trunc ('year','date').cast('date').alias('y'),
+              date_trunc ('month','date').cast('date').alias('m'),
+              dayofyear ('timestamp').alias('dy'),
+              dayofmonth('timestamp').alias('dm'),
+              dayofweek ('timestamp').alias('dw'),
+              quarter   ('timestamp').alias('q'),
+              month     ('timestamp').alias('m'),
+              hour      ('timestamp').alias('h'),
+            ).show(1)
