5fcc9385489d0aa21f78b3abd04c96c583d1ae85
rbizoi <razvan.bizoi@gmail.com>
2020-08-20 18:03:35 +0200

    Update 01-fonctions-integrees.scala

--- Module-06/08-fonctions-integrees/01-fonctions-integrees.scala
+++ Module-06/08-fonctions-integrees/01-fonctions-integrees.scala
@@ -1,82 +1,22 @@
 import org.apache.spark.sql.functions._
 
+val meteo = meteoDataFrame.select(
+                 col("numer_sta"),
+                 to_date(col("date").substr(0,8),"yyyyMMdd"),
+                 to_timestamp(col("date").cast("string"),"yyyyMMddHHmmss"),
+                 col("date").substr(0,4).cast("int"),
+                 col("date").substr(5,2).cast("int"),
+                 col("date").substr(7,2).cast("int"),
+                 col("date").substr(5,4),
+                 round(col("t") - 273.15,2),
+                 col("u") / 100 ,
+                 col("vv") / 1000 ,
+                 col("pres") / 1000,
+                 col("rr1")
+                 ).
+           toDF("id","date","timestamp","annee","mois","jour","mois_jour","temperature",
+                "humidite","visibilite","pression","precipitations").
+           cache()
 
-meteo.where("id < 8000").count()
-meteo.where("id < 8000").
-     select("annee","temperature", "humidite",
-             "visibilite", "pression","precipitations").
-     describe().
-     show()
-
-meteo.where("id < 8000").count()
-meteo.where("id < 8000").
-     where(col("temperature").isNotNull).
-     where(col("humidite").isNotNull ).
-     where(col("visibilite").isNotNull ).
-     where(col("pression").isNotNull ).
-     where(col("precipitations").isNotNull ).
-     count()
-
-meteo.where('id < 8000).count() - meteo.where('id < 8000).
-     where('temperature.isNotNull).
-     where('humidite.isNotNull ).
-     where('visibilite.isNotNull ).
-     where('pression.isNotNull ).
-     where('precipitations.isNotNull ).
-     count()
-
-meteo.where('id < 8000).
-    where('temperature.isNotNull).
-    where('humidite.isNotNull ).
-    where('visibilite.isNotNull ).
-    where('pression.isNotNull ).
-    count()
-
-meteo.where('id < 8000).
-      na.fill(0 ,Seq("precipitations")).
-      na.drop().
-      count()
-
-val meteoNotNull =  meteo.where('id < 8000).
-      na.fill(0 ,Seq("precipitations")).
-      na.drop()
-meteo.show()
-meteoNotNull.show()
-
-meteo.where("temperature is Null").count()
-meteo.filter(col("temperature").isNull).count()
-meteo.filter(col("temperature").isNull).count()
-meteo.filter('temperature.isNull).count()
-meteo.filter($"temperature".isNull).count()
-meteo.where($"temperature".isNull).count()
-
-meteo.where("humidite is Null").count()
-meteo.filter(col("humidite").isNull).count()
-meteo.filter(col("humidite").isNull).count()
-meteo.filter('humidite.isNull).count()
-meteo.filter($"humidite".isNull).count()
-meteo.where($"humidite".isNull).count()
-
-meteo.where("visibilite is Null").count()
-meteo.filter(col("visibilite").isNull).count()
-meteo.filter(col("visibilite").isNull).count()
-meteo.filter('visibilite.isNull).count()
-meteo.filter($"visibilite".isNull).count()
-meteo.where($"visibilite".isNull).count()
-
-meteo.where("pression is Null").count()
-meteo.filter(col("pression").isNull).count()
-meteo.filter(col("pression").isNull).count()
-meteo.filter('pression.isNull).count()
-meteo.filter($"pression".isNull).count()
-meteo.where($"pression".isNull).count()
-
-meteo.where("precipitations is Null").count()
-meteo.filter(col("precipitations").isNull).count()
-meteo.filter(col("precipitations").isNull).count()
-meteo.filter('precipitations.isNull).count()
-meteo.filter($"precipitations".isNull).count()
-meteo.where($"precipitations".isNull).count()
-
-
-meteo.na.fill(0 ,"precipitations").na.drop().show()
+meteo.select("date","timestamp","temperature","humidite",
+             "visibilite","pression","precipitations").show(3)
