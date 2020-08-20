b743c8c01287e1d86adab73210b7f63e0ab8b40d
rbizoi <razvan.bizoi@gmail.com>
2020-08-20 09:37:45 +0200

    Create 06-jointures.py

---
+++ Module-06/05-jointures/06-jointures.py
@@ -0,0 +1,18 @@
+from pyspark.sql.functions import *
+
+meteo.join(villes,
+       meteo.id == villes.Id)\
+       .select('ville','annee','mois_jour',
+               'temperature','precipitations')\
+       .show(10)
+
+meteo.join(villes,
+       meteo['id'].eqNullSafe(villes['Id']))\
+       .select('ville','annee','mois_jour',
+               'temperature','precipitations')\
+       .show(10)
+
+meteo.join(villes.withColumnRenamed('Id', 'id'),'id')\
+       .select('ville','annee','mois_jour',
+               'temperature','precipitations')\
+       .show(10)
