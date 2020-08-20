8f8bb74b241f2ddf6f30ee23446280489c60acd1
rbizoi <razvan.bizoi@gmail.com>
2020-08-20 09:38:33 +0200

    Delete 01-lignes.py

--- Module-06/01-colonnes-df/01-lignes.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import Row
-
-uneLigne = Row(1,"Isabelle","BIZOÏ")
-uneAutre = Row(2,"Razvan","BIZOÏ")
-listeLignes = [uneLigne,uneAutre]
-personnes = spark.createDataFrame(listeLignes,["Id","Prenom","Nom"])
-personnes.show()
