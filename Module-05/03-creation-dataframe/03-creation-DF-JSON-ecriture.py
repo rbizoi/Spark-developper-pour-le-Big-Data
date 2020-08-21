151e580176f86c8a52884f38d9991cf74623b3f2
rbizoi <razvan.bizoi@gmail.com>
2020-08-21 15:49:27 +0200

    Update 03-creation-DF-JSON-ecriture.py

--- Module-05/03-creation-dataframe/03-creation-DF-JSON-ecriture.py
+++ Module-05/03-creation-dataframe/03-creation-DF-JSON-ecriture.py
@@ -13,3 +13,18 @@ donnees.select ('nom_parking','libre','total').show(3)
 fichier = '/user/spark/donnees/json/parking_stras_json'
 format  = 'json'
 donnees.write.mode('overwrite').format(format).save(fichier)
+
+
+url = 'https://data.strasbourg.eu/api/records/1.0/search/?dataset='
+dataset = 'livraison_de_paniers'
+def ecrireJsonHDFS(url,dataset):
+    urlDS = url+dataset
+    urlDS = urlDS+"&q=&lang=fr&rows="+str(json.loads(urllib.request.urlopen(urlDS).read().decode())['nhits'])
+    donnees = spark.read.json(sc.parallelize(
+                [x['fields'] for x in
+                      json.loads(urllib.request.urlopen(urlDS
+                             ).read().decode())['records']
+                    ]))
+    fichier = '/user/spark/donnees/json/'+dataset
+    format  = 'json'
+    donnees.write.mode('overwrite').format(format).save(fichier)
