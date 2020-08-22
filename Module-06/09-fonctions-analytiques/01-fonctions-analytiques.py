83634d8c789914bc001f493b232615b6a5baca47
rbizoi <razvan.bizoi@gmail.com>
2020-08-23 00:07:41 +0200

    Update 01-fonctions-analytiques.py

--- Module-06/09-fonctions-analytiques/01-fonctions-analytiques.py
+++ Module-06/09-fonctions-analytiques/01-fonctions-analytiques.py
@@ -11,125 +11,107 @@ meteoFance.selectExpr('ville','annee','mois','jour','temperature as t',
                       'humidite as h','visibilite as v',
                       'pression as p','precipitations as e').show()
 
-
-
-
-
-val meteoFance = meteo.where("id < 8000").
-             join( villes.withColumnRenamed("Id", "id"),"id").
-             select(initcap(regexp_replace('ville,"-"," ")).alias("ville"),
-                     'annee,'mois,'jour,'temperature,
-                     'humidite,'visibilite,'pression,'precipitations)
-
-meteoFance.where("ville = 'Mont De Marsan' and "+
-                 "annee = 2019 and mois = 11 and jour < 11").
-           selectExpr("ville","annee","mois","jour","temperature as t",
-                      "humidite as h","visibilite as v",
-                      "pression as p","precipitations as e").
-           show()
-
-val meteoMM = meteoFance.where("ville = 'Mont De Marsan' and "+
-                                "annee = 2019 and "+
-                                "mois = 11 and jour < 11").
-                         select("jour","temperature",
-                                "humidite","visibilite",
-                                "pression","precipitations")
+meteoMM = meteoFance.where("ville = 'Mont De Marsan' and \
+                                annee = 2019 and mois = 11 and jour < 11")\
+                     .select('jour','temperature','humidite','visibilite',
+                             'pression','precipitations')
 meteoMM.show()
-meteoMM.groupBy("jour").
-        agg( round(sum("precipitations"),2).alias("precipitations")).
-        orderBy(col("jour")).
-        rollup("jour").
-        agg( round(sum("precipitations"),2).alias("precipitations")).
-        orderBy(col("jour").asc_nulls_last).
-        show()
-
-meteoMM.agg(sum("precipitations")).show()
+window = Window.partitionBy()
+meteoMM.select('jour','precipitations',
+               sum('precipitations').over(window)\
+                   .alias("somme totale"))\
+        .show(5)
 
 
 
-
-
-
-val meteoFance = meteo.where("id < 8000").
-             join( villes.withColumnRenamed("Id", "id"),"id").
-             select(initcap(regexp_replace('ville,"-"," ")).alias("ville"),
-                     'annee,'mois,'jour,'temperature,
-                     'humidite,'visibilite,'pression,'precipitations)
-
-meteoFance.where("ville = 'Mont De Marsan' and "+
-                 "annee = 2019 and mois = 11 and jour < 11").
-           selectExpr("ville","annee","mois","jour","temperature as t",
-                      "humidite as h","visibilite as v",
-                      "pression as p","precipitations as e").
-           show()
-
-val meteoMM = meteoFance.where("ville = 'Mont De Marsan' and "+
-                                "annee = 2019 and "+
-                                "mois = 11 and jour < 11").
-                         select("jour","temperature",
-                                "humidite","visibilite",
-                                "pression","precipitations")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+meteoMM.groupBy('jour')\
+        .agg( round(sum('precipitations'),2).alias('precipitations'))\
+        .orderBy(col('jour'))\
+        .rollup('jour')\
+        .agg( round(sum('precipitations'),2).alias('precipitations'))\
+        .orderBy(col('jour').asc_nulls_last())\
+        .show()
 
 from pyspark.sql import Window
-window = Window.partitionBy()
-meteoFance.where("ville = 'Strasbourg Entzheim'\
-                 and annee = 2020 and mois = 7")\
-          .select('annee','mois','jour','temperature',
-                   avg('temperature').over(window)).show()
+total = Window.partitionBy()
+jour  = Window.partitionBy('jour')
+meteoMM.select('jour','precipitations',
+               round(sum('precipitations').over(jour),2)\
+                   .alias("somme journalière"),\
+               sum('precipitations').over(total)\
+                   .alias("somme totale"))\
+        .show()
+
+
+meteoMM = meteoFance.where("ville = 'Mont De Marsan'")\
+           .select('ville','annee','mois',
+                           'jour','temperature','precipitations')\
+           .groupBy('ville', 'annee', 'mois', 'jour')\
+           .agg( round(avg('temperature'),2).alias('temperature'),
+                round(sum('precipitations'),2).alias('precipitations'))\
+           .select('annee','mois','jour','temperature','precipitations')
+meteoMM.show(5)
+
+jour          = Window.partitionBy('jour')
+mois          = Window.partitionBy('mois')
+moisAnnee     = Window.partitionBy('mois','annee')
+annee         = Window.partitionBy('annee')
+jourMois      = Window.partitionBy('jour','mois')
+jourMoisAnnee = Window.partitionBy('jour','mois','annee')
+
+meteoMM.select('jour','mois','annee',col('precipitations').alias('prec'),
+          round(sum('precipitations').over(jourMoisAnnee),2).alias('s1'),
+          round(sum('precipitations').over(jourMois),2).alias('s2'),
+          round(sum('precipitations').over(moisAnnee),2).alias('s3'),
+          round(sum('precipitations').over(mois),2).alias('s4'),
+          round(sum('precipitations').over(annee),2).alias('s5'),
+          round(sum('precipitations').over(jour),2).alias('s6'))\
+       .show(28)
+
+meteoMM.where('annee = 1997 and mois = 12 and jour = 31').agg(sum('precipitations').alias('s1')).show()
+meteoMM.where('mois = 12 and jour = 31').agg(sum('precipitations').alias('s2')).show()
+meteoMM.where('annee = 1996 and mois = 12').agg(sum('precipitations').alias('s3')).show()
+meteoMM.where('mois  = 12'  ).agg(sum('precipitations').alias('s4')).show()
+meteoMM.where('annee = 1996').agg(sum('precipitations').alias('s5')).show()
+meteoMM.where('jour = 31').agg(sum('precipitations').alias('s6')).show()
+meteoMM.agg(sum('precipitations')).show()
+
+
+meteoMM = meteoFance.where("ville = 'Mont De Marsan' \
+                            and annee = 2019")\
+           .select('ville','annee','mois',
+                           'jour','temperature','precipitations')\
+           .groupBy('ville', 'annee', 'mois', 'jour')\
+           .agg( round(avg('temperature'),2).alias('temperature'),
+                round(sum('precipitations'),2).alias('precipitations'))\
+           .select('mois','jour','temperature','precipitations')\
+           .orderBy('mois','jour')
+meteoMM.show(5)
+
+jour = Window.partitionBy('mois').orderBy('jour')
+meteoMM.select('jour','mois','annee',col('precipitations').alias('prec'),
+          round(sum('precipitations').over(jourMoisAnnee),2).alias('s1'),
+          round(sum('precipitations').over(jourMois),2).alias('s2'),
+          round(sum('precipitations').over(moisAnnee),2).alias('s3'),
+          round(sum('precipitations').over(mois),2).alias('s4'),
+          round(sum('precipitations').over(annee),2).alias('s5'),
+          round(sum('precipitations').over(jour),2).alias('s6'))\
+       .show(28)
 
 
 
 
-meteo = meteoDataFrame.select(
-                 col('numer_sta'),
-                 col('date')[0:4].cast('int') ,
-                 col('date')[5:2].cast('int'),
-                 col('date')[7:2].cast('int'),
-                 col('date')[5:4],
-                 round(col('t') - 273.15,2),
-                 col('u') / 100 ,
-                 col('vv') / 1000 ,
-                 col('pres') / 1000,
-                 col('rr1')*3,
-                 col('rr3'),
-                 col('rr6')/2,
-                 col('rr12')/4,
-                 col('rr24')/8)\
-                   .toDF('id','annee','mois','jour','mois_jour','temperature',
-                   'humidite','visibilite','pression',
-                   'precipitations1','precipitations3','precipitations6',
-                   'precipitations12','precipitations24')\
-             .cache()
 
 
 
-meteo.select('id','precipitations1','precipitations3','precipitations6',
-      'precipitations12','precipitations24','temperature').describe().show()
 
 
-meteo.select( coalesce('precipitations3','precipitations24',
-                       'precipitations12','precipitations6',
-                       'precipitations1').alias('precipitations')).show()
 
 
-meteo.select( coalesce('precipitations24','precipitations12',
-                       'precipitations6','precipitations3',
-                       'precipitations1').alias('precipitationsH')).describe().show()
 
 
-meteo.count(),meteo.where(meteo['precipitations3'].isnull()).count()
+
+
+
+
+       
