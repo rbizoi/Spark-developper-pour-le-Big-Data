14ecc440e2aefc0b7e411f470377abc2603468f8
rbizoi <razvan.bizoi@gmail.com>
2020-08-24 00:18:48 +0200

    Create 01-create-database.sql

---
+++ Module-07/01-SQL-DDL/01-create-database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE
+     IF NOT EXISTs coursSpark3
+     LOCATION
+     "hdfs://jupiter.olimp.fr:8020/user/spark/databases/coursspark3_db";
+
+CREATE DATABASE
+    IF NOT EXISTs coursSpark3
+    LOCATION
+    "hdfs:///user/spark/databases/coursspark3_db";
+
+DROP DATABASE  cours_spark CASCADE;
