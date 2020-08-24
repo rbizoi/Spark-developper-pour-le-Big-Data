36a3346941d81027fedd9a876ccc1a0c0ac8b0db
rbizoi <razvan.bizoi@gmail.com>
2020-08-24 19:06:53 +0200

    Update 03-access-aux-donees.py

---
+++ Module-07/01-SQL-DDL/03-access-aux-donees.py
@@ -0,0 +1,8 @@
+
+spark.sql('show databases').show()
+spark.sql('use gest_comm')
+
+spark.sql("""SELECT MOIS,NB_COMMANDES,PORT,QUANTITE
+            FROM clientsQuantites
+            WHERE ANNEE = 2019 AND CLIENT = "La corne d'abondance"
+            ORDER BY MOIS""").show()
