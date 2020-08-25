13cd52a6a38f5c8cd8c6c6b8dd13cdd061b06f50
rbizoi <razvan.bizoi@gmail.com>
2020-08-25 13:52:39 +0200

    Update 02-create-views.scala

--- Module-07/01-SQL-DDL/02-create-views.scala
+++ Module-07/01-SQL-DDL/02-create-views.scala
@@ -4,7 +4,7 @@ val spark = SparkSession.builder.
               enableHiveSupport().
               getOrCreate()
 
-val donnees = spark.sql("select * from parquet.`donnees/meteoFrance`")
+val donnees = spark.sql("select * from parquet.`donnees/meteoFrance`").cache()
 donnees.createOrReplaceGlobalTempView("GVmeteoFrance")
 donnees.createOrReplaceTempView("TVmeteoFrance")
 
@@ -12,6 +12,13 @@ spark.sql("select  ville,mois,jour,temperature,humidite,"+
            "visibilite,pression from TVmeteoFrance").show(3)
 spark.sql("select  ville,mois,jour,temperature,humidite,"+
            "visibilite,pression from global_temp.GVmeteoFrance").show(3)
+
+val autreSession = spark.newSession()
+autreSession.sql("select  ville,mois,jour,temperature,humidite,"+
+          "visibilite,pression from global_temp.GVmeteoFrance").show(3)
+
+autreSession.sql("select  ville,mois,jour,temperature,humidite,"+
+          "visibilite,pression from TVmeteoFrance").show(3)
 
 donnees.write.
         mode("overwrite").
