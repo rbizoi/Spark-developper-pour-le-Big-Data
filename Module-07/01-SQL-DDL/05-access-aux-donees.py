88d70a2efae3971cbb06f01032375b9c0bfb8efe
rbizoi <razvan.bizoi@gmail.com>
2020-08-25 15:54:30 +0200

    Delete 05-access-aux-donees.py

--- Module-07/01-SQL-DDL/05-access-aux-donees.py
+++
@@ -1,7 +0,0 @@
-spark.sql('show databases').show()
-spark.sql('use gest_comm')
-
-spark.sql("""SELECT MOIS,NB_COMMANDES,PORT,QUANTITE
-            FROM clientsQuantites
-            WHERE ANNEE = 2019 AND CLIENT = "La corne d'abondance"
-            ORDER BY MOIS""").show()
