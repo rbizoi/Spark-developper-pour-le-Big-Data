3f54604ca1dd6d35366cdbd4a2714643bd5faa7f
rbizoi <razvan.bizoi@gmail.com>
2020-08-26 20:15:15 +0200

    Update 03-groupements.sql

--- Module-07/03-projections-restrictions/03-groupements.sql
+++ Module-07/03-projections-restrictions/03-groupements.sql
@@ -1,97 +1,114 @@
-spark.sql("""
-
-""").show(truncate=False)
-spark.sql("show databases").show()
-spark.sql("use gest_comm").show()
-
-
-SELECT NOM, PRENOM, REND_COMPTE FROM EMPLOYES WHERE REND_COMPTE = 18;
-SELECT NOM, PRENOM, SALAIRE FROM EMPLOYES WHERE REND_COMPTE IS NULL;
-SELECT NOM, PRENOM, SALAIRE FROM EMPLOYES WHERE SALAIRE > 13000;
-SELECT NOM_PRODUIT, NO_FOURNISSEUR FROM PRODUITS WHERE NO_FOURNISSEUR <= 2;
-SELECT NOM, PRENOM, FONCTION FROM EMPLOYES WHERE FONCTION <>'Représentant(e)' ;
-SELECT NOM, PRENOM, FONCTION FROM EMPLOYES WHERE FONCTION != 'Représentant(e)' ;
-SELECT NOM_PRODUIT, QUANTITE FROM PRODUITS WHERE QUANTITE LIKE '%boîtes%kg%' ;
-SELECT NOM_PRODUIT, QUANTITE FROM PRODUITS WHERE QUANTITE LIKE '___%pièces' ;
-SELECT NOM_PRODUIT, QUANTITE FROM PRODUITS WHERE QUANTITE LIKE '___pièces' ;
-CREATE TABLE PRODUITS_LIKE(NOM_PRODUIT STRING, QUANTITE STRING);
-INSERT INTO PRODUITS_LIKE VALUES
-  ("Fruit Cocktail","430_g"),
-  ("Chocolate Biscuits Mix","10 boîtes_de_12_pièces"),
-  ("Marmalade","30 boîtes%cadeau"),
-  ("Scones","24 paquets de 4_pièces");
-SELECT NOM_PRODUIT, QUANTITE FROM PRODUITS_LIKE   WHERE QUANTITE LIKE '%/%%' ESCAPE '/'
-SELECT NOM_PRODUIT, QUANTITE FROM PRODUITS_LIKE   WHERE QUANTITE LIKE '%#_%' ESCAPE '#'
-SELECT QUANTITE FROM PRODUITS WHERE RLIKE (QUANTITE,'^(10).*(pièces)$');
-SELECT QUANTITE FROM PRODUITS WHERE RLIKE (QUANTITE,'^2.*(carton|pièces|bouteilles)');
-SELECT QUANTITE FROM PRODUITS WHERE RLIKE (QUANTITE,'^[^1-3]');
-SELECT QUANTITE FROM PRODUITS WHERE RLIKE(QUANTITE,"^\\d{2,3}\\s\\w{3,6}\\s[\\(]\\d{1,3}\\s\\w?(g[\\)])$");
-SELECT NOM_PRODUIT, NO_FOURNISSEUR FROM PRODUITS  WHERE NO_FOURNISSEUR BETWEEN 2 AND 3;
-SELECT NOM_PRODUIT, NO_FOURNISSEUR FROM PRODUITS  WHERE NO_FOURNISSEUR IN (1,3,6,8);
-SELECT VILLE FROM VILLES WHERE VILLE IN ('Paris','Strasbourg','Toulouse') ;
-SELECT NOM, FONCTION, SALAIRE FROM EMPLOYES WHERE SALAIRE > 2500 AND NOT FONCTION LIKE 'Rep%';
-SELECT NOM, FONCTION, SALAIRE FROM EMPLOYES WHERE SALAIRE > 2500 AND FONCTION NOT LIKE 'Rep%';
-SELECT NOM,PRENOM, TITRE  FROM EMPLOYES WHERE TITRE NOT IN ('M.','Mme');
-
-SELECT NOM_PRODUIT, QUANTITE,NO_FOURNISSEUR,CODE_CATEGORIE
-FROM PRODUITS
-WHERE QUANTITE LIKE '%boîtes%'
-      AND  NO_FOURNISSEUR BETWEEN 2 AND 6
-      AND  CODE_CATEGORIE = 2;
-
-SELECT NOM_PRODUIT, QUANTITE,NO_FOURNISSEUR,CODE_CATEGORIE
-FROM PRODUITS
-WHERE QUANTITE LIKE '%boîtes%'
-      AND  ( NO_FOURNISSEUR BETWEEN 2 AND 6
-             OR   CODE_CATEGORIE = 2 ) ;
+show databases
+use gest_comm
 
 SELECT COUNT(*) AS C1, COUNT(NO_COMMANDE) AS C2,
       COUNT(DISTINCT NO_COMMANDE) AS C3, COUNT(DATE_PAIEMENT) AS C4
 FROM factures;
 
+SELECT ROUND(SUM(t),2) as SUM,
+       ROUND(AVG(t),2) as AVG,
+       ROUND(MIN(t),2) as MIN,
+       ROUND(MAX(t),2) as MAX,
+       ROUND(COUNT(t),2) as COUNT,
+       ROUND(STDDEV(t),2) as STDDEV ,
+       ROUND(STDDEV_SAMP(t),2) as STDDEV_S,
+       ROUND(VARIANCE(t),2) as VARIANCE ,
+       ROUND(VAR_POP(t),2) as VAR_POP,
+       ROUND(COVAR_POP(t,rr3),2) as COVAR_P,
+       ROUND(COVAR_SAMP(t,rr3),2) as COVAR_S
+FROM coursspark3.meteoinitialep
 
-spark.sql("""
-""").show(120,truncate=False)
+SELECT * FROM
+(SELECT station,
+  annee,
+  mois,
+  ROUND(AVG(temperature),2) as temp,
+  ROUND(SUM(COALESCE(precipitations3,
+    precipitations24/8,
+    precipitations12/4,
+    precipitations6/2,
+    precipitations1*3)),2) as prec,
+    ROUND(AVG(humidite),2) as hum
+    FROM meteopartitionannee
+    GROUP BY station,annee,mois
+    ORDER BY station,annee DESC,mois) LIMIT 5;
 
+SELECT station,
+        annee,
+        mois,
+        ROUND(AVG(temperature),2) as temp,
+        ROUND(SUM(COALESCE(precipitations3,
+                     precipitations24/8,
+                     precipitations12/4,
+                     precipitations6/2,
+                     precipitations1*3)),2) as prec,
+        ROUND(AVG(humidite),2) as hum
+FROM meteopartitionannee
+GROUP BY station,annee,mois
+HAVING AVG(temperature) > 29
+       AND AVG(humidite) > 82;
 
+ SELECT * FROM
+   (SELECT station,
+         annee,
+         mois,
+         ROUND(AVG(temperature),2) as temp,
+         ROUND(SUM(COALESCE(precipitations3,
+                      precipitations24/8,
+                      precipitations12/4,
+                      precipitations6/2,
+                      precipitations1*3)),2) as prec,
+         ROUND(AVG(humidite),2) as hum
+  FROM meteopartitionannee
+  GROUP BY ROLLUP(station,annee,mois)
+  ORDER BY station,annee DESC,mois) LIMIT 200;
+  SELECT * FROM
+  (SELECT station,
+        annee,
+        mois,
+        ROUND(AVG(temperature),2) as temp,
+        ROUND(SUM(COALESCE(precipitations3,
+                     precipitations24/8,
+                     precipitations12/4,
+                     precipitations6/2,
+                     precipitations1*3)),2) as prec,
+        ROUND(AVG(humidite),2) as hum
+FROM meteopartitionannee
+WHERE annee > 2018
+GROUP BY CUBE(station,annee,mois)
+ORDER BY station,annee DESC,mois) LIMIT 120;
 
+SELECT station,
+        annee,
+        mois,
+        ROUND(AVG(temperature),2) as temp,
+        ROUND(SUM(COALESCE(precipitations3,
+                     precipitations24/8,
+                     precipitations12/4,
+                     precipitations6/2,
+                     precipitations1*3)),2) as prec,
+        ROUND(AVG(humidite),2) as hum
+FROM meteopartitionannee
+WHERE station < 7050
+  and annee   > 2018
+  and mois    < 5
+GROUP BY GROUPING SETS(station,annee,(annee,mois))
+ORDER BY station,annee,mois;
 
-
-
-
-'^\\d{2,3}\\s\\w{3,6}\s[\(]\d{1,3}\s\w?(g[\)])$');
-
-SELECT QUANTITE FROM PRODUITS
-WHERE RLIKE (QUANTITE,'(100).*\1');
-
-
-SELECT * FROM person WHERE name LIKE '%$_%' ESCAPE '$';
-
-
-
-SELECT NOM_PRODUIT, NO_FOURNISSEUR FROM PRODUITS
-WHERE NO_FOURNISSEUR <= 2;
-
-
-
-spark.sql("""
-""").show(120,truncate=False)
-
-spark.sql("""
-""").show(120,truncate=False)
-
-
-spark.sql("""
-""").show(120,truncate=False)
-
-
-spark.sql("""
-""").show(120,truncate=False)
-
-
-
-
-
-
-SELECT QUANTITE FROM PRODUITS
-WHERE REGEXP_LIKE (QUANTITE,'^(10).*(pièces)$');
+SELECT  GROUPING_ID(station,annee,mois) AS GI,
+station,
+annee,
+mois,
+ROUND(AVG(temperature),2) as temp,
+ROUND(SUM(COALESCE(precipitations3,
+  precipitations24/8,
+  precipitations12/4,
+  precipitations6/2,
+  precipitations1*3)),2) as prec,
+  ROUND(AVG(humidite),2) as hum
+  FROM meteopartitionannee
+  WHERE station < 7050
+  and annee   > 2018
+  and mois    < 5
+  GROUP BY CUBE(station,annee,mois)
+  ORDER BY station,annee DESC,mois
