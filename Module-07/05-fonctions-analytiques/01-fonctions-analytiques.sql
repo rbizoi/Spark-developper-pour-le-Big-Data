6c4bd96472180b4068e9a2601d01051171493d52
rbizoi <razvan.bizoi@gmail.com>
2020-08-27 21:30:34 +0200

    Update 01-fonctions-analytiques.sql

--- Module-07/05-fonctions-analytiques/01-fonctions-analytiques.sql
+++ Module-07/05-fonctions-analytiques/01-fonctions-analytiques.sql
@@ -387,3 +387,38 @@ SELECT pays  ,
                       AND 1 FOLLOWING) AS lv2
 FROM sr_initiale
 ORDER BY pays,annee,mois;
+
+WITH sr_initiale (
+  SELECT vl.pays                                      ,
+         EXTRACT ( year  FROM date_commande) AS annee ,
+         EXTRACT ( month FROM date_commande) AS mois  ,
+         SUM(port)                           AS port
+  FROM COMMANDES CO
+          JOIN details_commandes dc
+               ON dc.no_commande = co.no_commande
+          JOIN acheteurs ac
+               ON co.no_acheteur = ac.no_acheteur
+          JOIN magasins mg          ON ac.no_magasin  = mg.no_magasin
+          JOIN adresses ad          ON mg.no_adresse  = ad.no_adresse
+          JOIN villes vl            ON ad.no_ville    = vl.no_ville
+  WHERE EXTRACT ( year  FROM date_commande) IN (2017,2018)
+    AND EXTRACT ( month FROM date_commande) <= 5
+    AND vl.pays IN ('Allemagne','France')
+  GROUP BY  vl.pays,
+            EXTRACT ( year  FROM date_commande) ,
+            EXTRACT ( month FROM date_commande)
+)
+SELECT pays  ,
+       annee ,
+       mois  ,
+       port  ,
+       LAG(port,1,-9999) OVER ( PARTITION BY pays
+                        ORDER BY annee,mois ) AS lag1,
+       LAG(port,3,-9999) OVER ( PARTITION BY pays
+                        ORDER BY annee,mois ) AS lag2,
+       LEAD(port,1,-9999) OVER ( PARTITION BY pays
+                        ORDER BY annee,mois ) AS lead1,
+       LEAD(port,3,-9999) OVER ( PARTITION BY pays
+                        ORDER BY annee,mois ) AS lead2
+FROM sr_initiale
+ORDER BY pays,annee,mois;
